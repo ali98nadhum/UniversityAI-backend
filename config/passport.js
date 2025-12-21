13e53e97df43cbaa8d7c414ce08144c72ebff129
ali98nadhum <alinadhum1998@gmail.com>
2025-12-21 10:00:56 +0300

    Add config passport lib 🎫

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await prisma.user.findUnique({
+          where: { googleId: profile.id },
+        });
+
+        if (!user) {
+          user = await prisma.user.create({
+            data: {
+              googleId: profile.id,
+              email: profile.emails[0].value,
+              name: profile.displayName,
+              avatar: profile.photos[0].value,
+            },
+          });
+        }
+
+        done(null, user);
+      } catch (err) {
+        done(err, null);
+      }
+    }
+  )
+);
+
+module.exports = passport;
