6420922efb243bdac8945fe07a5c6930ff07a5ea
ali98nadhum <alinadhum1998@gmail.com>
2025-12-25 23:14:52 +0300

    Add faq controllers

---
+++ controllers/Admin/FaqControllers.js
@@ -0,0 +1,33 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { pipeline, cos_sim } = require("@xenova/transformers");
+
+
+
+
+
+// ==================================
+// @desc Create new Faq
+// @route /api/v1/faq
+// @method POST
+// @access private ( just admin )
+// ==================================
+module.exports.createFaq = async (req, res) => {
+  try {
+    const { question, answer, keywords } = req.body;
+    const vector = await getVector(question);
+    const faq = await prisma.fAQ.create({
+      data: {
+        question,
+        answer,
+        keywords: keywords || [],
+        embedding: vector,
+      },
+    });
+
+    res.json({ message: "Success", id: faq.id });
+  } catch (error) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to save FAQ" });
+  }
+};
