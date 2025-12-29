8bc3e6ba7987b37d08d95e6c69ec7b07429dc070
ali98nadhum <alinadhum1998@gmail.com>
2025-12-29 08:52:40 +0300

    Update chat controllers

--- controllers/User/ChatControllers.js
+++ controllers/User/ChatControllers.js
@@ -1,16 +1,4 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
-const path = require("path");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
-// استيراد مكتبة الذكاء الاصطناعي المجانية
-const { pipeline, cos_sim } = require("@xenova/transformers");
-
-const passport = require("./config/passport");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
 const { askQuestion } = require("../../helpers/semanticSearch");
 
 
